ed5fe23c154cdf67925fa03c1d314dbbfbfd2a1d
Hashim khan <84334197+Hashimkhann1@users.noreply.github.com>
2022-09-03 16:07:28 +0500

    UI updated

--- lib/screens/register.dart
+++ lib/screens/register.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_1/screens/login.dart';
 
 class MyRegister extends StatefulWidget {
   const MyRegister({Key? key}) : super(key: key);
@@ -11,7 +12,133 @@ class _MyRegisterState extends State<MyRegister> {
   @override
   Widget build(BuildContext context) {
     return Container(
-      child: Text('jksbajvasi'),
+      decoration: BoxDecoration(
+        image: DecorationImage(
+          image: AssetImage('images/register.png'),
+          fit: BoxFit.cover
+        )
+      ),
+      child: Scaffold(
+        backgroundColor: Colors.transparent,
+        body: Stack(
+          children: [
+            Container(
+              padding: EdgeInsets.only(left: 36.0, top: 100.0),
+              child: Text(
+                'Create\nAccount',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 34.0,
+                  fontWeight: FontWeight.w600,
+                ),
+              ),
+            ),
+            Container(
+              padding: EdgeInsets.only(top: MediaQuery.of(context).size.height * 0.4, left: 20.0 , right: 20.0),
+              child: Column(
+                children: [
+                  TextField(
+                    decoration: InputDecoration(
+                        hintText: 'Full Name',
+                        hintStyle: TextStyle(
+                            color: Colors.white
+                        ),
+                        filled: true,
+                        fillColor: Colors.transparent,
+                        focusedBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(12.0),
+                            borderSide: BorderSide(color: Colors.white)
+                        ),
+                        enabledBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(12.0),
+                            borderSide: BorderSide(color: Colors.white)
+                        )
+                    ),
+                  ),
+                  SizedBox(
+                    height: 10.0,
+                  ),
+                  TextField(
+                    decoration: InputDecoration(
+                        hintText: 'Email',
+                        hintStyle: TextStyle(
+                            color: Colors.white
+                        ),
+                        filled: true,
+                        fillColor: Colors.transparent,
+                        focusedBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(12.0),
+                            borderSide: BorderSide(color: Colors.white)
+                        ),
+                      enabledBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(12.0),
+                          borderSide: BorderSide(color: Colors.white)
+                      )
+                    ),
+                  ),
+                  SizedBox(
+                    height: 10.0,
+                  ),
+                  TextField(
+                    decoration: InputDecoration(
+                        hintText: 'Password',
+                        hintStyle: TextStyle(
+                            color: Colors.white
+                        ),
+                        filled: true,
+                        fillColor: Colors.transparent,
+                        focusedBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(12.0),
+                            borderSide: BorderSide(color: Colors.white)
+                        ),
+                        enabledBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(12.0),
+                            borderSide: BorderSide(color: Colors.white)
+                        )
+                    ),
+                  ),
+                  SizedBox(height: 26.0,),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text('Sign In',
+                      style: TextStyle(
+                        fontSize: 28.0,
+                        fontWeight: FontWeight.w800,
+                        color: Colors.white
+                      ),),
+                      CircleAvatar(
+                        radius: 30.0,
+                        backgroundColor: Color(0xff4c505b),
+                        child: IconButton(
+                          onPressed: (){},
+                          icon: Icon(Icons.arrow_forward),
+
+                        ),
+                      )
+                    ],
+                  ),
+                  SizedBox(height: 16.0,),
+                  Row(
+                    children: [
+                      TextButton(
+                        onPressed: (){Navigator.pushNamed(context, 'login');},
+                        child: Text('Sign In',
+                        style: TextStyle(
+                          fontSize: 20.0,
+                          fontWeight: FontWeight.w700,
+                          color: Colors.white,
+                          decoration: TextDecoration.underline
+                        ),),
+                      ),
+                    ],
+                  )
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
     );
   }
 }
